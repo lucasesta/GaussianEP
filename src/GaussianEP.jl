3e17a645fd31b60b2dd020709ea26a888c700e8a
luca sesta <luca@nb-sesta.polito.it>
2021-06-25 12:40:53 +0200

    Code to implement MC simulation

--- src/GaussianEP.jl
+++ src/GaussianEP.jl
@@ -1,13 +1,14 @@
 module GaussianEP
 
-export expectation_propagation, Term, EPState, EPOut
-export Prior, IntervalPrior, SpikeSlabPrior, BinaryPrior, GaussianPrior, PosteriorPrior, QuadraturePrior, AutoPrior, ThetaPrior
+export expectation_propagation, Term, TermRBM, EPState, EPOut
+export Prior, IntervalPrior, SpikeSlabPrior, BinaryPrior, GaussianPrior, PosteriorPrior, QuadraturePrior, AutoPrior, ThetaPrior, UniformPrior
 
-using ExtractMacro, SpecialFunctions, LinearAlgebra
+using ExtractMacro, SpecialFunctions, LinearAlgebra, DelimitedFiles
 
 include("Term.jl")
 include("priors.jl")
 include("expectation_propagation.jl")
 include("ProgressReporter.jl")
+include("MC_rbm.jl")
 
 end # end module
